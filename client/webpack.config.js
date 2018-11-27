0c047951fdb7fa610c7221b1a4833dd31cf1521c
Aprianto Wiwarsono <anto.wiwarsono@bepure.co.nz>
2018-11-28 08:17:23 +1300

    Init frontend

---
+++ client/webpack.config.js
@@ -0,0 +1,50 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const BUILD_DIR = path.resolve(__dirname, 'public');
+const APP_DIR = path.resolve(__dirname, 'src');
+
+module.exports = {
+  context: APP_DIR,
+  entry: './index.jsx',
+  output: {
+    filename: 'bundle.js',
+    path: BUILD_DIR,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+      },
+      {
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  devServer: {
+    contentBase: BUILD_DIR,
+    hot: true,
+  },
+};
