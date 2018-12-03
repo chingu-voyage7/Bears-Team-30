c9edbdd933fc1334818dcc6f02177df5b829a804
Muhammad Amir <m.amir.mugal@gmail.com>
2018-12-03 21:48:08 +1100

    Added passport and refined the code

--- server/init/express.js
+++ server/init/express.js
@@ -1,18 +1,10 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-// view engine setup
-app.set('views', path.join(__dirname, '../views'));
-app.set('view engine', 'pug');
+const logger = require('morgan');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../public')));
 
 module.exports = app;
