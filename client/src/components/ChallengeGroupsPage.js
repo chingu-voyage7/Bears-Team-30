3b82fcd6b5c82291821b1e530acc93073d89f0dd
mawarijo <mdopure@gmail.com>
2018-12-12 23:12:58 -0800

    show join group page when group selected

--- client/src/components/ChallengeGroupsPage.js
+++ client/src/components/ChallengeGroupsPage.js
@@ -10,6 +10,8 @@ class ChallengeGroupsPage extends React.Component {
   state = {
     category: '',
     query: '',
+    showJoinChallenge: false,
+    challengeGroup: null,
   };
 
   onCategoryChange = e => {
@@ -24,23 +26,41 @@ class ChallengeGroupsPage extends React.Component {
     this.setState(() => ({ query }));
   };
 
+  onChallengeSelect = e => {
+    e.preventDefault();
+    const challengeGroupId = e.target.value;
+
+    const challengeGroup = CHALLENGE_GROUPS.filter(
+      group => group.id === challengeGroupId
+    )[0];
+
+    this.setState(() => ({ showJoinChallenge: true, challengeGroup }));
+  };
+
   render() {
     return (
       <div>
-        <h2>Find a Challenge to Join</h2>
-        <SearchBar
-          onQueryChange={this.onQueryChange}
-          value={this.state.query}
-        />
-        <CategorySelect
-          onCategoryChange={this.onCategoryChange}
-          value={this.state.category}
-        />
-        <ChallengeGroupsList
-          category={this.state.category}
-          onChallengeSelect={this.onChallengeSelect}
-          query={this.state.query}
-        />
+        {!this.state.showJoinChallenge && (
+          <div>
+            <h2>Find a Challenge to Join</h2>
+            <SearchBar
+              onQueryChange={this.onQueryChange}
+              value={this.state.query}
+            />
+            <CategorySelect
+              onCategoryChange={this.onCategoryChange}
+              value={this.state.category}
+            />
+            <ChallengeGroupsList
+              category={this.state.category}
+              onChallengeSelect={this.onChallengeSelect}
+              query={this.state.query}
+            />
+          </div>
+        )}
+        {this.state.showJoinChallenge && (
+          <JoinChallengeGroupPage challengeGroup={this.state.challengeGroup} />
+        )}
       </div>
     );
   }
---
+++ client/src/components/JoinChallengeGroupPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+// TODO: show page title "My Challenge", challenge group name and description, goal setup with initial value set to challenge group defaults (goalAction, goalNumber, goalType)
+
+const JoinChallengeGroupPage = props => <div>Join Challenge</div>;
+
+export default JoinChallengeGroupPage;
