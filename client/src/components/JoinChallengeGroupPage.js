37454faa92192e2afc23fcbfae0ee9f9d8527c08
mawarijo <mdopure@gmail.com>
2018-12-14 21:53:57 -0800

    add challenge info, goal section to join challenge group page

--- client/src/components/JoinChallengeGroupPage.js
+++ client/src/components/JoinChallengeGroupPage.js
@@ -1,7 +1,52 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
-// TODO: show page title "My Challenge", challenge group name and description, goal setup with initial value set to challenge group defaults (goalAction, goalNumber, goalType)
+import * as routes from '../constants/routes';
+import CHALLENGE_GROUPS from '../constants/challengeGroups';
+import GoalSection from './GoalSection';
 
-const JoinChallengeGroupPage = props => <div>Join Challenge</div>;
+class JoinChallengeGroupPage extends React.Component {
+  state = {
+    goalNumber: 0,
+  };
 
-export default JoinChallengeGroupPage;
+  componentDidMount() {
+    const { goalNumber } = this.props.challengeGroup;
+    this.setState(() => ({ goalNumber }));
+  }
+
+  onGoalNumberChange = e => {
+    const goalNumber = e.target.value;
+    this.setState(() => ({ goalNumber }));
+  };
+
+  onSettingsSave = e => {
+    e.preventDefault();
+
+    const { goalNumber } = this.state;
+    console.log('goalNumber: ', goalNumber);
+
+    this.props.history.push(routes.DASHBOARD);
+  };
+
+  render() {
+    const { challengeGroup } = this.props;
+    return (
+      <div>
+        <h3>Edit Your Challenge Settings</h3>
+        <h4>{challengeGroup.name}</h4>
+        <p>{challengeGroup.description}</p>
+        <GoalSection
+          challengeGroup={challengeGroup}
+          onChange={this.onGoalNumberChange}
+          value={this.state.goalNumber}
+        />
+        <button type="button" onClick={this.onSettingsSave}>
+          Save My Settings
+        </button>
+      </div>
+    );
+  }
+}
+
+export default withRouter(JoinChallengeGroupPage);
