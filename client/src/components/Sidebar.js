5c4143055377e3f922818bada21639ed707cf551
mawarijo <mdopure@gmail.com>
2018-12-18 08:13:53 -0800

    Sidebar loads username from me query

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
-const Sidebar = ({ onClick }) => (
+const Sidebar = ({ data }) => (
   <div>
     <h1>100 Days</h1>
-    <h5>Hello!</h5>
+    <h5>{data && data.me && data.me.username}</h5>
     <h5>My Challenges</h5>
   </div>
 );
 
-export default Sidebar;
+export default graphql(gql`
+  query ME {
+    me {
+      username
+    }
+  }
+`)(Sidebar);
