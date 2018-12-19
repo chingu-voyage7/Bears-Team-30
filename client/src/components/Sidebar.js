df1e6dacddd2cc78d69afeb8fd586b25c07900f6
mawarijo <mdopure@gmail.com>
2018-12-18 22:44:47 -0800

    add user challenges list in sidebar

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -2,11 +2,13 @@ import React from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 
+import UserChallengesList from './UserChallengesList';
+
 const Sidebar = ({ data }) => (
   <div>
     <h1>100 Days</h1>
-    <h5>{data && data.me && data.me.username}</h5>
-    <h5>My Challenges</h5>
+    <h2>{data && data.me && data.me.username}</h2>
+    <UserChallengesList />
   </div>
 );
 
---
+++ client/src/components/UserChallengesList.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+
+const GET_MY_CHALLENGES = gql`
+  {
+    myChallenges {
+      createdAt
+      progress
+      goal
+      challengeGroup {
+        name
+        goalType
+      }
+    }
+  }
+`;
+
+const UserChallengesList = ({ onClick }) => (
+  <Query query={GET_MY_CHALLENGES}>
+    {({ loading, error, data }) => {
+      if (loading) return 'Loading...';
+      if (error) return `Error! ${error.message}`;
+
+      return (
+        <div>
+          <h3>My Challenges</h3>
+          {data.myChallenges.length > 0
+            ? data.myChallenges.map(challenge => (
+                <button onClick={onClick}>
+                  <h4>{challenge.challengeGroup.name}</h4>
+                  <p>
+                    {challenge.progress}/{challenge.goal}{' '}
+                    {challenge.challengeGroup.goalType}
+                  </p>
+                </button>
+              ))
+            : 'No Challenges Joined'}
+        </div>
+      );
+    }}
+  </Query>
+);
+
+export default UserChallengesList;
