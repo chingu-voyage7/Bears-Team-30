e0fe94871a950049eb4942306ae270b29f1d5bf9
mawarijo <mdopure@gmail.com>
2018-12-22 14:01:28 -0800

    Sidebar header links back to dashboard

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 
+import * as routes from '../constants/routes';
 import UserChallengesList from './UserChallengesList';
 
 const Sidebar = ({ data }) => (
   <div>
-    <h1>100 Days</h1>
+    <Link to={routes.DASHBOARD}>
+      <h1>100 Days</h1>
+    </Link>
     <h2>{data && data.me && data.me.username}</h2>
     <UserChallengesList />
   </div>
